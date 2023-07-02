2c2d257cf5630c987501ece861d7db131680aa60
Samuel Kalu <kalusam619@gmail.com>
2023-07-02 06:01:12 +0100

    Update love.py

--- love.py
+++ love.py
@@ -1,31 +1,51 @@
 from turtle import *
 
-# Set up turtle environment
 bgcolor('#9fa')
 color('red')
 shape('turtle')
-title('First turtle program')
-shapesize(10, 10)
+title('First turtle program 🐢🐢')
+shapesize(10,10)
 fd(0)
-shapesize(9, 9)
+shapesize(9,9)
 fd(0)
-shapesize(8, 8)
+shapesize(8,8)
 fd(0)
+shapesize(7,7)
+fd(0)
+shapesize(6,6)
+fd(0)
+shapesize(5,5)
+fd(0)
+shapesize(4,4)
+fd(0)
+shapesize(3,3)
+fd(0)
+shapesize(2,2)
+fd(0)
+shapesize(1,1)
 
-
-# Draw a shape
+#begin fill 
 begin_fill()
 left(50)
 forward(150)
-circle(80, 180)
+circle(80,180)
 right(100)
-circle(80, 180)
+circle(80,180)
 forward(150)
 forward(41)
 end_fill()
 
+#CODE FOR DISPLAYING THE TEXT
 
-# Draw another shape
+up()
+left(140)
+forward (180)
+left(90)
+forward(80)
+right(90)
+down()
+
+
 bgcolor('orange')
 pencolor('white')
 width(4)
@@ -35,21 +55,37 @@ forward(30)
 up()
 forward(25)
 down()
-
 circle(20)
 up()
 forward(50)
 down()
 
-# Draw another shape
+#for v
+left(60)
+forward(30)
+left(120)
+up()
+forward (30)
+left(100)
+down()
+
+forward(30)
+up()
+left(80)
+forward(60)
+
+down()
 bgcolor('#222')
-circle(13, -270)
+
+
+#for e
+circle(13,-270)
 left(90)
 forward(26)
 up()
 right(90)
 forward(200)
+#ending statement
 
-# Ending statement
 bgcolor('pink')
 done()
