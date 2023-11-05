5b06d0c47c02c74407bd24cbbdc5a5cd317cc015
Swaminathan Kannan <65940566+SwamiKannan@users.noreply.github.com>
2023-11-05 09:57:37 +0530

    made into a single file

--- turtle-race.py
+++ turtle-race.py
@@ -1,56 +1,71 @@
 from turtle import Turtle, Screen
-import random
-import time
-ALIGNMENT="center"
-FONT=("Courier", 20, "normal")
-
-def Screen_setup():
-    screen.setup(width=500, height=400)
-    screen.title("Turtle Race")
-    
-def game():
-    is_race_on = False
-    game_over = Turtle()
-    game_over.hideturtle()
-    game_over.goto(0,0)
-    Screen_setup()
-    colors =['violet','indigo','blue','green','yellow','orange','red']
-    user_bet = screen.textinput("Make Your Bet",f"Which turtle will win the race: ? Enter a color? {tuple(colors)} ").lower()
-
-    y_start = [180,120,60,0,-60,-120,-180]
-    all_turtles =[]
-
-    if user_bet in colors:
-        is_race_on = True
-    else:
-        screen.bye()
-        
-    for i in range(len(colors)):
-        new_turtle = Turtle(shape='turtle')
-        new_turtle.color(colors[i])
-        new_turtle.penup()
-        new_turtle.goto(x=-220,y=y_start[i])
-        all_turtles.append(new_turtle)
-    while is_race_on:
-        for turtle in all_turtles:
-            if turtle.xcor()> 230:
-                is_race_on = False
-                winner =turtle.pencolor()
-                if user_bet == winner:
-                    game_over.write(f"You've won!\nThe {winner} turtle has won!",align=ALIGNMENT,font=FONT)
-                else:
-                    game_over.write(f"You lost your bet!\nThe {winner} turtle won!",align=ALIGNMENT,font=FONT)
-            movement = random.randint(0,10)
-            turtle.forward(movement)
-
+import random, time
+from tkinter import *
+from tkinter import messagebox
+final_text=''
 screen = Screen()
-Screen_setup()
+scr_width=800
+scr_height=600
+screen.setup(width=scr_width, height=scr_height)
+turtles = []
+color = ['red', 'blue', 'green', 'yellow', 'orange', 'black', 'purple']
+interval = 30
+y_end = -70
+color_text = ', '.join([col.capitalize() for col in color])
 
-while screen.textinput("Turtle Race", "Do you want to bet on Turtle Race? y/n:").lower() == "y":
-    game()
-    time.sleep(3)
-    screen.clearscreen()
+correct_color = False
+while not correct_color:
+    bet = screen.textinput('Welcome to Turtle Races ! ', "Which Turtle would you like to bet on?: "+color_text)
+    if bet.lower() not in color:
+        bet = screen.textinput('Welcome to Turtle Races ! ',
+                               "Incorrect choice ! Which Turtle would you like to bet on?: "+color_text)
+    else:
+        correct_color=True
+        break
+for i in range(len(color)):
+    turt = Turtle(shape='turtle')
+    turt.color(color[i])
+    turt.penup()
+    y_pos=y_end+interval*i
+    turt.setpos(-400, y_pos)
+    turtles.append(turt)
+
+if correct_color:
+    race_on = True
+else:
+    race_on=False
+#print(race_on)
+for i in reversed(range(4)):
+    col=turt.color()[0]
+    turt.write(str(i+1), font=('Arial, 48'))
+    time.sleep(1)
+    turt.color('white')
+    turt.write(str(i + 1), font=('Arial, 48'))
+    turt.color(col)
+while race_on:
+    for turt in turtles:
+        steps = random.randint(0, 10)
+        turt.forward(steps)
+        if turt.xcor() > (scr_width/2)-10: #turtle size is 20 so 10 is half that (center of mass)
+            race_on = False
+            turt_winner = turt.color()
+            result_text=f'The winner is the {turt.color()[0]} turtle'
+
+final_text+=f'\n\nYou bet on the {bet} turtle'
+final_text+=f'\n\nThe winner was the {turt_winner[0].capitalize()} turtle'
+if turt_winner[0] == bet:
+    title_text='You won!'
+    final_text+='\n\nYou won the bet !'
+else:
+    title_text='You lost!'
+    final_text+='\n\nYou lost the bet :('
 
 
-screen.bye()
-screen.exitonclick()
+#from tkinter import * 
+from tkinter import messagebox
+  
+# root = Tk()
+#messagebox.showinfo('Final Results of the Race', result_text) # Results
+messagebox.showinfo(result_text, final_text) # The alert.
+  
+#root.mainloop() 
